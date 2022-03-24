d45beb1a4099a609f1139a52f5ad231ceb9b6bf5
Anastasiya Volkova <86426424+tasoniy@users.noreply.github.com>
2022-03-24 19:49:26 +0300

    Update js.js

--- modul 10/10.1/js.js
+++ modul 10/10.1/js.js
@@ -1,3 +1,7 @@
+/* Сверстайте кнопку, которая будет содержать в себе icon_01 (как в примере в последнем видео). 
+При клике на кнопку иконка должна меняться на icon_02. Повторный клик меняет иконку обратно.
+*/
+
 const button = document.getElementById('button');
 const empty = document.getElementById ('empty');
 const filled = document.getElementById('filled');
